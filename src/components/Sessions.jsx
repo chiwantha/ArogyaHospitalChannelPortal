8712032eb688a686c76161ea3b2a5f5b11666ccf
Kasun Chiwantha <kasunchiwantha789@gmail.com>
2025-07-02 15:24:06 +0530

    20250702

--- src/components/Sessions.jsx
+++ src/components/Sessions.jsx
@@ -1,15 +1,33 @@
-import React from "react";
 import SessionBar from "./Static/SessionBar";
+import { useQuery } from "@tanstack/react-query";
+import { makeRequest } from "../axios";
+import { useParams } from "react-router-dom";
 
 const Sessions = () => {
+  const { id: doctor_id } = useParams();
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["sessionList"],
+    queryFn: async () => {
+      const res = await makeRequest.get(
+        `/doctors/sessions?doctor_id=${doctor_id}`
+      );
+      return res;
+    },
+  });
+
   return (
     <div className="flex flex-col gap-4">
-      <SessionBar />
-      <SessionBar />
-      <SessionBar />
-      <SessionBar />
-      <SessionBar />
-      <SessionBar />
+      {isLoading && !data ? (
+        <div></div>
+      ) : error ? (
+        error
+      ) : (
+        data &&
+        data.data.map((item, index) => (
+          <SessionBar key={index} session_data={item} />
+        ))
+      )}
     </div>
   );
 };
--- src/components/Static/SessionBar.jsx
+++ src/components/Static/SessionBar.jsx
@@ -1,70 +1,56 @@
-import React from "react";
 import Button from "./Button";
-import { Company } from "../../constants";
 
-const SessionBar = ({
-  startTime,
-  endTime,
-  fee,
-  doctorId,
-  sessionId,
-  session,
-}) => {
+const SessionBar = ({ session_data }) => {
+  const { day, start_time, end_time, type, fee } = session_data;
   return (
     <div
-      className="w-full grid md:grid-cols-4 rounded-lg shadow-md border bg-slate-50 
-     hover:border-[#0460D9]/50 border-gray-300 transition-colors duration-300 grid-cols-1 "
+      className="relative w-full grid grid-cols-2 md:grid-cols-4
+      rounded-lg shadow-md border bg-slate-50 border-gray-300
+       hover:border-[#0460D9]/50 transition-colors duration-300 p-4
+       space-y-4 md:space-y-0"
+      role="region"
+      aria-label="Session Details"
     >
-      {/* HospitalImage */}
-      {/* <div className="md:rounded-l-lg md:rounded-r-none rounded-lg bg-[#FFC935] p-2 flex items-center justify-center">
-        <img
-          src={Company.logo}
-          width={100}
-          height={100}
-          className="aspect-auto  object-center "
-          alt="logo.png"
-        />
-      </div> */}
-      <div
-        className="flex flex-col items-center justify-center
-        border-gray-300 sm:border-r"
-      >
-        <span className="text-xl font-black text-[#0560D9]">Monday</span>
+      {/* Left Indicator */}
+      <span
+        className="absolute left-0 top-1/2 -translate-y-1/2 w-2 h-8 rounded-r bg-[#4DB847]"
+        aria-hidden="true"
+      />
+
+      {/* Day */}
+      <div className="flex flex-col items-center justify-center border-gray-300 sm:border-r col-span-2 sm:col-span-1">
+        <span className="text-lg font-extrabold text-gray-600">
+          {day ? day : "Day"}
+        </span>
       </div>
-      <div
-        className="col-span-3 grid grid-cols-2 sm:grid-cols-3 p-4 space-y-4
-      md:space-y-0"
-      >
-        {/* Time */}
-        <div
-          className="flex items-center flex-col justify-center
-        border-gray-300 border-r"
-        >
-          <span className="text-sm text-gray-700  font-medium">Time</span>
-          <span className="text-[18px] font-black text-[#0560D9]">
-            15:00 : 17:00
-          </span>
-          <span className="text-sm font-semibold text-gray-600">
-            Morning Session
-          </span>
-        </div>
-        {/* Prices */}
-        <div
-          className="flex flex-col items-center justify-center
-        border-gray-300 sm:border-r"
-        >
-          <span className="text-sm font-medium text-gray-700">Fee</span>
-          <span className="text-[18px] font-bold text-green-600">
-            Rs 2600.00
-          </span>
-        </div>
-        {/* Book */}
-        <div className="flex flex-col items-center justify-center col-span-full sm:col-span-1">
-          <Button
-            title={"Book Now"}
-            bg={"bg-[#4DB847] text-white  px-2 hover:bg-green-600 py-1"}
-          />
-        </div>
+
+      {/* Time */}
+      <div className="flex flex-col items-center justify-center border-gray-300 border-r sm:border-0 md:border-r">
+        <span className="text-sm font-medium text-gray-700">Time</span>
+        <span className="text-lg font-extrabold text-[#0560D9]">
+          {start_time && end_time
+            ? start_time.slice(0, 5) + " - " + end_time.slice(0, 5)
+            : "00:00 - 00:00"}
+        </span>
+        <span className="text-sm font-semibold text-gray-600">
+          {type ? type : "Session Type"}
+        </span>
+      </div>
+
+      {/* Price */}
+      <div className="flex flex-col items-center justify-center border-gray-300 sm:border-r">
+        <span className="text-sm font-medium text-gray-700">Fee</span>
+        <span className="text-lg font-bold text-gray-600">
+          {fee ? fee : "Rs 00.00"}
+        </span>
+      </div>
+
+      {/* Book Now */}
+      <div className="flex flex-col items-center justify-center col-span-2 mt-2 sm:mt-0 sm:col-span-1">
+        <Button
+          title="Book Now"
+          bg="bg-[#4DB847] text-white px-2 py-1 hover:bg-green-600 rounded-lg"
+        />
       </div>
     </div>
   );
