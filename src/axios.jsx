3cce2a5171747a2649828d9a0ae38ebc6c697934
Kasun Chiwantha <kasunchiwantha789@gmail.com>
2025-07-08 01:30:46 +0530

    20250708 Build

--- src/axios.jsx
+++ src/axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: "http://192.168.8.101:8800/server",
+  baseURL: "https://portal.arogyahospitals.lk/server",
   withCredentials: true,
 });
